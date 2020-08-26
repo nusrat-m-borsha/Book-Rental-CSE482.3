21830f9fd317bf8a2b6cfa6262ceaa302bbe918d
nusrat-m-borsha <nusrat.barsha@northsouth.edu>
2020-08-26 21:41:20 +0600

    update index file

--- backend-2/public/index.php
+++ backend-2/public/index.php
@@ -1,131 +1,77 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	<title>Book Rental</title>
-	<link rel="stylesheet" type="text/css" href="homepage.css">
-	<link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet'>
-	<link href='https://fonts.googleapis.com/css?family=Source Sans Pro' rel='stylesheet'>
-	<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet'>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="media_queries.css">
-    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
-</head>
-<body>
 
-   <!----------------------------------Navbar----------------------------------->
-   <header>
-	<nav class="navbar navbar-expand-sm">
-		<!-- Brand/logo -->
-		<div class="nav-logo">
-		<a class="navbar-brand" href="#">Book Rental</a>
-	   </div>
-		<!-- Links -->
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-		  <span class="navbar-toggler-icon"><i class="fa fa-bars"></i>
-		  </span>
-		</button>
-		<div class="collapse navbar-collapse" id="collapsibleNavbar">
-		<ul class="navbar-nav ml-auto">
-		  <li class="nav-item">
-			<a class="nav-link" href="#">Browse Books</a>
-		  </li>
-		  <li class="nav-item">
-			<a class="nav-link" href="#">Sign up</a>
-		  </li>
-		  <li class="nav-item">
-			<a class="nav-link" href="#">Login</a>
-		  </li>
-		  <li class="nav-item">
-			<a class="nav-link" href="payment.html">My Cart</a>
-		  </li>
-		</ul>
-		</div>
-	</nav>
-</header>
+<?php require_once("../resources/config.php"); 
+    include_once('header.php');
+?>
+
+
     <!----------------------------------Below Navbar----------------------------------->
-<div class="below-navbar">
+<div class="container text-center ">
 	<div class="greeting">
 	<h1>Welcome Bibliophiles!</h1>
-	</div>
-	<div class="below-navbar-paragraph">
-	<p>The shelves are all booked up for you!</p>
+	<h5>The shelves are all booked up for you!</h5>
 	</div>
 </div>
 	<!----------------------------------Search Container------------------------------->
-	<div class="container"></div>
-	  <div class="row">
-		<div class="col-md-1"></div>
-		<div class="col-xs-12 col-sm-12 col-md-10 ">
-            <div class="search-container">
-             <form>
-  		       <input class="search" type="text" placeholder="Search..."/>
- 		       <button class="button">Search</button>    
-			</form>
-			</div>
-		</div>
-		<div class="col-md-1"></div>
-	  </div>
-	</div>
-	<!----------------------------------Books------------------------------------------>
-<div class="books">
-	<div class="bookheadline">
-		<h2>Latest Collection</h2>
-	</div>
-	<hr/>
-	<div class="container"></div>
-	  <div class="row">
-		<div class="col-md-1"></div>
-		<div class="col-xs-12 col-sm-12 col-md-10 ">
-	        <div class="book-images">
-		        <img src="img/1.png" alt="Book 1">
-		        <img src="img/2.png" alt="Book 2">
-		        <img src="img/3.png" alt="Book 3">
-		        <img src="img/4.png" alt="Book 4">
-		        <img src="img/5.png" alt="Book 5">
-		        <img src="img/6.png" alt="Book 6">
-		        <img src="img/7.png" alt="Book 7">
-		        <img src="img/8.png" alt="Book 8">
-		        <img src="img/10.png" alt="Book 10">
-		        <img src="img/11.png" alt="Book 11">
-	</div>
+<div class="container">
+  <div class="row">
+		<div class="input-group mb-3">
+		  <input type="text" class="form-control" placeholder="Search">
+		  <div class="input-group-append">
+		    <button class="btn btn-success" type="submit">Search</button>
+		  </div>
+		</div>	
+  </div>
 </div>
+	<!----------------------------------Books------------------------------------------>
+
+       
+
+<div class="container"> <!-- Book images -->
+
+  <h1 class="font-weight-light text-center text-lg-left mt-4 mb-0">Latest Collections</h1>
+
+  <hr class="mt-2 mb-5">
+
+  <div class="row text-center text-lg-left">
+
+  	<?php
+    $sql = "SELECT book_id, book_image, book_title, author, ISBN FROM book";
+    $result = mysqli_query($connection, $sql);
+    if (mysqli_num_rows($result) > 0) {
+	?>
+
+	<?php while($row = mysqli_fetch_assoc($result))
+        {
+        ?>
+
+
+	    <div class="col-lg-3 col-md-4 col-6">
+	      <?php echo "<a href='single_book.php?id={$row['book_id']}' class=\"d-block mb-4 h-100 text-center\">"; ?>
+	            <?php echo '<img src="data:image;base64,'.base64_encode($row['book_image']).'" alt="Image" style="width:200px; height:250px;">'; ?>
+	            <p class="text-center book-title"><strong><?php echo $row["book_title"];?> </strong></p>
+	          </a>
+	    </div>
+
+    <?php
+    }
+    }
+    else
+    echo "<center>No books found in your collection </center>" ;
+    ?>
+    
+
+  </div>
+
+</div>
+
+
 	<!----------------------------------Footer----------------------------------------->
 
-<footer>
-<div class="panel-footer-2 text-center">
-  <div class="container">
-    <div class="row">
-      <section id="hours" class="col-xs-12 col-sm-12 col-md-4">
-        <span>Hours:</span><br>
-        <p>Sun-Thurs: 10:00am - 6.00pm</p>
-        <p>Dhaka, Bangladesh</p>
-        <hr class="visible-xs">
-      </section>
-      <section id="address" class="col-xs-12 col-sm-12 col-md-4">
-        <span>Contact:</span><br>
-        <p>Email: bookrentalsystem@gmail.com</p>
-        <p>Phone: +88 01711 111 111</p>
-        <hr class="visible-xs">
-      </section>
-      <section id="social" class="col-xs-12 col-sm-12 col-md-4">
-        <span>Social Media: </span><br> 
-        <a href="" class="fa fa-facebook"></a>
-        <a href="#" class="fa fa-instagram"></a>
-      </section>
-    </div>
-    <div class="text-center">&copy; Book Rental System 2020</div>
-  </div>
-  </div>
-
-</footer>
 </body>
+
+
+
+  <?php include_once('footer.php');?>
 </html>
 
-</body>
-</html>
+
